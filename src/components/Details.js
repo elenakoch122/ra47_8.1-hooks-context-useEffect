4073cbdd6c4d3767e48aeef858201ef9e7cfd57d
Elena Kochegarova <lelet@ngs.ru>
2023-05-07 18:17:56 +0700

    finished

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,36 @@
-export default function Details({ user }) {
-  if (!user) return;
+import { useEffect, useState } from "react";
+
+export default function Details({ userId }) {
+  const [loading, setLoadling] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      setUser(null);
+      setLoadling(true);
+
+      try {
+        await fetch(process.env.REACT_APP_URL + `${userId}.json`)
+          .then(res => res.json())
+          .then(json => setUser(json));
+      } finally {
+        setLoadling(false);
+      }
+    }
+
+    fetchData();
+  }, [userId]);
 
   return (
-    <section className="profile">
-      <img className="avatar" src={user.avatar} alt="" />
-      <h1 className="profile__item name">{user.name}</h1>
-      <p className="profile__item city">City: {user.details.city}</p>
-      <p className="profile__item company">Company: {user.details.company}</p>
-      <p className="profile__item position">Position: {user.details.position}</p>
-    </section>
+    <div>
+      {loading && <p>Loading...</p>}
+      {user && <section className="profile">
+        <img className="avatar" src={user.avatar} alt="" />
+        <h1 className="profile__item name">{user.name}</h1>
+        <p className="profile__item city">City: {user.details.city}</p>
+        <p className="profile__item company">Company: {user.details.company}</p>
+        <p className="profile__item position">Position: {user.details.position}</p>
+      </section>}
+    </div>
   );
 }
